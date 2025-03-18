67192fce545ad8c2896448dc68ec98ed356f0e3b
Angelika Brown <angelikpbrown@gmail.com>
2025-03-18 17:00:48 -0400

    Okie

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -6,19 +6,25 @@ class Parity(enum.Enum):
     ODD = 0
     EVEN = 1
 
-
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     """
     Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
     what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
     updating this here docstring to something useful.
 
+    Based on parity (odd or even), return a list from the start and end arguments.
+
     :param start:
     :param stop:
     :param parity:
     :return:
     """
-    pass
+    output = []
+    for num in range(start, stop):
+        if parity == 0:
+             output.append(num)
+    return output
+
 
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
