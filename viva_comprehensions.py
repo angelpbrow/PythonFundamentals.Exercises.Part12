9dd09995bd6cceb4c52b52c27c9ae0878f8b9828
Angelika Brown <angelikpbrown@gmail.com>
2025-03-18 17:22:16 -0400

    I comprehend. Now to code it.

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -33,6 +33,9 @@ def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
     updating this here docstring to something useful.
 
+        Based on strategy return a dictionary start and not including stop integers as key pairs with its result
+        strategy 1 is squaring the int
+        strategy 2 is getting the factorial
 
     :param start:
     :param stop:
@@ -47,6 +50,11 @@ def gen_set(val_in: str) -> Set:
     Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
     what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
     updating this here docstring to something useful.
+
+    return a set that takes the lowercase letters in a string and turns them into uppercase. Not sure what the last
+    one is doing. it takes all uppercase string and returns an empty set?
+    found this example in the book.
+    set comprehension.
 
     :param val_in:
     :return:
